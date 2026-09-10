28a8377f9c8c5a137a1ae341a744abd127e63c51
Evgenii Dmitrievich (Kiselev) <e.kiselev@umai.kg>
2026-09-10 14:38:45 +0600

    Header сайта

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
+const LanguageStorageKey = "Language";
 
+function getCurrentLang(){
+    return localStorage.getItem(LanguageStorageKey) || "ru";
+}
 
+const currentLang = getCurrentLang();
+document.querySelector(`#lang-switch option[value=${currentLang}]`).selected = true;
+switchLanguage(currentLang)
 
+const langSwitch = document.querySelector("#lang-switch");
+langSwitch.onchange = (event) => {
+    switchLanguage(event.target.value);
+}
+
+function switchLanguage(targetLang) {
+    localStorage.setItem(LanguageStorageKey, targetLang);
+    const translatableItems = [...document.querySelectorAll("*")].filter(x => x.dataset.translate);
+    translatableItems.forEach(item => {
+        item.innerText = translations[item.dataset.translate][targetLang] || "?";
+    });
+}
+
+document.querySelector('#get-price').onclick = () => {
+    const text = translations["WhatsAppMessage"][getCurrentLang()];
+    const textQuery = encodeURIComponent(text)
+    const whatsAppUrl = `https://wa.me/996501720006?text=${textQuery}`
+    window.open(whatsAppUrl, "_blank");
+}
---
+++ scripts/translations.js
@@ -0,0 +1,42 @@
+const translations = {
+    "MainPage": {
+        "ru": "Главная",
+        "kg": "Башкы бет",
+        "en": "Main page",
+    },
+    "Catalog": {
+        "ru": "Каталог",
+        "kg": "Каталог",
+        "en": "Catalog",
+    },
+    "Solutions": {
+        "ru": "Решения",
+        "kg": "Чечимдер",
+        "en": "Solutions",
+    },
+    "About": {
+        "ru": "О компании",
+        "kg": "Компания жөнүндө",
+        "en": "About us",
+    },
+    "Cases": {
+        "ru": "Кейсы",
+        "kg": "Кейстер",
+        "en": "Cases",
+    },
+    "Contacts": {
+        "ru": "Контакты",
+        "kg": "Байланыш маалыматы",
+        "en": "Contacts",
+    },
+    "GetPrice": {
+        "ru": "Получить прайс",
+        "kg": "Прайс алыңыз",
+        "en": "Get price",
+    },
+    "WhatsAppMessage": {
+        "ru": "Здравствуйте, пишу с вашего сайта.",
+        "kg": "Саламатсызбы, мен сиздердин веб-сайтыңыз аркылуу жазып жатам.",
+        "en": "Hello. I'm writing you from website.",
+    },
+}
